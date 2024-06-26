741bd6cec8da17d251cdc7b4dc18db933237f36d
ShawtyCarrotz <152631559+ShawtyCarrotz@users.noreply.github.com>
2024-06-26 17:17:52 +0100

    Add band name generator

---
+++ Band name generator-/main.py
@@ -0,0 +1,4 @@
+print("Welcome! Let's give you a proper name.")
+color = input("What is your favorite color?\n")
+animal = input("Whatś your favorite animal?\n")
+print("Your band name is " + color + " " + animal + "! You're welcome noob.")
